22af40d11a98a44a0dafe7c4963f560120ca4982
Miguel-laptop <miguel.teranj02@gmail.com>
2022-10-31 17:48:33 -0400

    Desafio OpenBootcamp

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-console.log(
-  "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo"
-);
+// function usersNames() {
+//   const arrs = new Array();
+//   const mssge1 = arrs.push("Eduardo", "Gabriel", "Nicolas", "Miguel");
+
+//   console.log(mssge1);
+// }
+
+// usersNames();
+
+var name = "Miguel";
+var age = 21;
+var developer = true;
+var born = new Date("02 / 04 / 2001");
+var favourBook = {
+  title: "El Arte de la Guerra",
+  author: "Sun Tzu",
+  description:
+    "Un libro bastante interesante que te muestra guías sobre cómo aumentar tus posibilidades de obtener ventajas sobre tus enemigos",
+};
