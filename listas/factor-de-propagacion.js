94b47ef6a2199b203b27fd2896a811bb8b14fd5e
Miguel Teran <miguel.teranj02@gmail.com>
2022-12-05 13:26:46 -0400

    desafio de listas en javascript casi terminado

--- listas/factor-de-propagacion.js
+++ listas/factor-de-propagacion.js
@@ -6,3 +6,18 @@ console.log(...greet);
 let greetings = "hello";
 let chars = [...greetings];
 console.log(chars);
+
+// Lista de Subscriptores
+let blog1Subs = ["[email protected]", "[email protected]"];
+let blog2Subs = ["[email protected]", "[email protected]", "[email protected]"];
+
+let subs = [...blog1Subs, ...blog2Subs];
+
+console.log(subs);
+
+// empujar el contenido de un Array a otro
+let arr1 = ["john", "Sofia", "Bob"];
+let arr2 = ["Julia", "Sean", "Anthony"];
+
+arr1.push(...arr2);
+console.log(arr1);
---
+++ listas/metodo concat().js
@@ -0,0 +1,16 @@
+// Concatenates two arrays
+const letters = ["a", "b", "c"];
+const numbers = [1, 2, 3];
+
+const alphaNumeric = letters.concat(numbers);
+
+console.log(alphaNumeric);
+
+// Concatenate three arrays
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+const num3 = [7, 8, 9];
+
+const concatNumbers = num1.concat(num2, num3);
+
+console.log(concatNumbers);
